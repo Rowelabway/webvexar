41cfe4049a6b5184bd7ded269242c643bf0f6fc8
Rowelabway <santiagoagk@gmail.com>
2023-11-06 15:34:09 +0100

    grafica

---
+++ js/popup.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const telegramLink = document.querySelector(".telegram-link");
+    const twitterLink = document.querySelector(".twitter-link");
+
+    telegramLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        const popup = document.createElement("div");
+        popup.className = "popup";
+        popup.innerText = "Coming Soon";
+        document.body.appendChild(popup);
+        setTimeout(() => {
+            popup.classList.add("fade-out");
+            setTimeout(() => {
+                document.body.removeChild(popup);
+            }, 3000); // Ocultar el popup después de 3 segundos
+        }, 10); // Agregar una pequeña demora para la animación
+    });
+
+    twitterLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        const popup = document.createElement("div");
+        popup.className = "popup";
+        popup.innerText = "Coming Soon";
+        document.body.appendChild(popup);
+        setTimeout(() => {
+            popup.classList.add("fade-out");
+            setTimeout(() => {
+                document.body.removeChild(popup);
+            }, 3000); // Ocultar el popup después de 3 segundos
+        }, 10); // Agregar una pequeña demora para la animación
+    });
+});
